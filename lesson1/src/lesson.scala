336bcc7b97dd128a9c567418067f23e6a8589880
537419 <537419@PC264894.cts.com>
2016-02-03 17:23:15 +0530

    hi

--- lesson1/src/lesson.scala
+++ lesson1/src/lesson.scala
@@ -1,10 +1,11 @@
 
-case class Book(var title: String, var noOfPages: Int)
+/* case class Book(var title: String, var noOfPages: Int)
 
 object HelloWorld{
 	def greet(){
 		println("Hello World!")
 		}
 	HelloWorld.greet()
-	}
+	} */
 
+println("Hello World");
