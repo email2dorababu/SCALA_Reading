1bb8c3589d1487bc98de3101347383b54f701f3d
537419 <537419@PC264894.cts.com>
2016-02-11 18:12:02 +0530

    commit

---
+++ REST/RESTfulWebService/src/com/service/user/UserServices.java
@@ -0,0 +1,13 @@
+package com.service.user;
+
+
+@Path("/user/service")
+public class UserServices {
+	
+	@PUT	
+	public void createUser(){}
+		public void getUser(){}
+		public void updateUser(){}
+		public void deleteUser(){}
+	
+}
