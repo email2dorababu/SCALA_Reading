2098210b7d8d600f1f03a293e118fdf9834d4f4b
537419 <537419@PC264894.cts.com>
2016-02-12 18:15:15 +0530

    commit

--- REST/RESTfulWebService/src/com/service/user/UserServices.java
+++ REST/RESTfulWebService/src/com/service/user/UserServices.java
@@ -1,13 +1,33 @@
 package com.service.user;
 
+import javax.ws.rs.*;
 
-@Path("/user/service")
-public class UserServices {
+
+@Path("/user/service")  
+public class UserServices { //RESOURCE CLASS , associated with @Path
 	
-	@PUT	
-	public void createUser(){}
-		public void getUser(){}
-		public void updateUser(){}
-		public void deleteUser(){}
+	@PUT
+	@Path("/create")
+	public void createUser(){} // SERVICE RESOURCE METHOD Method have it's own @Path AND http methods 
+	
+	@GET
+	public void getUser(){ // normal resource methods are just with http methods.
+		System.out.println("Inside getUser method");
+		
+	}  
+	
+	@POST
+	public void updateUser(){
+		System.out.println("Inside updateUser method");
+	}
+	
+	@DELETE
+	public void deleteUser(){}
+	
+	@Path("/special")
+	public SpecialUser getSpecialUser(){ //SUBRESOURCE LOCATOR METHOD - have only @Path, no http methods
+										//purpose is to return another RESOURCE CLASS, called SUB RESOURCE CLASS 
+		return new SpecialUser();
+	}
 	
 }
